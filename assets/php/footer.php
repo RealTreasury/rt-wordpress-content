cad04025fb784ea4c026aca3deccaf0799e6a0e7
RealTreasury <tschultz@realtreasury.com>
2025-07-01 12:20:33 -0400

    Create footer.php

---
+++ assets/php/footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php
+	astra_content_after();
+
+	astra_footer_before();
+
+	astra_footer();
+
+	astra_footer_after();
+?>
+	</div><!-- #page -->
+<?php
+	astra_body_bottom();
+	wp_footer();
+?>
+<!-- Video Access Modal -->
+<div id="videoModal" class="modal" style="display: none;">
+    <div class="modal-content">
+        [contact-form-7 id="0779c74" title="Video Access Form"]
+    </div>
+</div>
+	</body>
+</html>

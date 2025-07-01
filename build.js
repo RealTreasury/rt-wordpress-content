2d6de5e2f28206b1f4255295f88609f7fe379694
RealTreasury <tschultz@realtreasury.com>
2025-07-01 12:40:01 -0400

    Add templating build system for insights

---
+++ build.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+
+const templatesDir = path.join(__dirname, 'templates');
+
+function ensureDir(dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+}
+
+function* walk(dir) {
+  for (const entry of fs.readdirSync(dir, { withFileTypes: true })) {
+    const res = path.resolve(dir, entry.name);
+    if (entry.isDirectory()) {
+      if (entry.name === 'partials') continue;
+      yield* walk(res);
+    } else if (entry.isFile() && res.endsWith('.html')) {
+      yield res;
+    }
+  }
+}
+
+for (const file of walk(templatesDir)) {
+  const rel = path.relative(templatesDir, file);
+  const dest = path.join(__dirname, rel);
+  const template = fs.readFileSync(file, 'utf8');
+  const html = ejs.render(template, {}, { filename: file, root: templatesDir });
+  ensureDir(path.dirname(dest));
+  fs.writeFileSync(dest, html);
+  console.log('Rendered', dest);
+}

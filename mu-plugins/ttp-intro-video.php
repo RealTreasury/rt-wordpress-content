34bea3f28e6910b40b8e3ef7117edc39b682fabc
RealTreasury <tschultz@realtreasury.com>
2025-08-13 18:03:03 -0400

    Use external intro video URL

---
+++ mu-plugins/ttp-intro-video.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Define the intro video URL for the Treasury Tech Portal.
+ */
+if (!defined('TTP_INTRO_VIDEO_URL')) {
+    define('TTP_INTRO_VIDEO_URL', 'https://realtreasury.com/wp-content/uploads/2024/06/Portal-Intro.mp4');
+}
+

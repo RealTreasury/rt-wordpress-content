b9f59d4d103f07b87eb80add4f6bdde95d2fa352
RealTreasury <tschultz@realtreasury.com>
2025-08-14 08:24:36 -0400

    chore: update intro video URL for 2025

--- mu-plugins/ttp-intro-video.php
+++ mu-plugins/ttp-intro-video.php
@@ -3,6 +3,6 @@
  * Define the intro video URL for the Treasury Tech Portal.
  */
 if (!defined('TTP_INTRO_VIDEO_URL')) {
-    define('TTP_INTRO_VIDEO_URL', 'https://realtreasury.com/wp-content/uploads/2024/06/Portal-Intro.mp4');
+    define('TTP_INTRO_VIDEO_URL', 'https://realtreasury.com/wp-content/uploads/2025/08/Portal-Intro.mp4');
 }
 

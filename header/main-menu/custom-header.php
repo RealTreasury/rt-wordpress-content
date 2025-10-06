d7de57c393ad8248d945866d0d975239dba67166
RealTreasury <213981846+RealTreasury@users.noreply.github.com>
2025-10-06 11:02:12 -0400

    Align Treasury Systems naming

--- header/main-menu/custom-header.php
+++ header/main-menu/custom-header.php
@@ -33,7 +33,7 @@ function add_my_custom_header_html() {
                                 <div class="rt-main-menu-column">
                                     <a href="https://realtreasury.com/treasury-tech-market/" class="rt-explore-link">
                                         <div class="chart-title-overlay">
-                                            <h3>Treasury &amp; Risk Management Systems</h3>
+                                            <h3>Treasury Systems</h3>
                                             <p>North America</p>
                                         </div>
                                         <img class="rt-explore-image"
